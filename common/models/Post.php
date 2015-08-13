47b3b5d18a9ee7550f36a8038c3c7d36677c3172
Alexander Makarov <sam@rmcreative.ru>
2015-08-13 23:51:10 +0300

    Post model

---
+++ common/models/Post.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $body
+ * @property string $link
+ * @property integer $user_id
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ *
+ * @property User $user
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    const STATUS_DELETED = 0;
+    const STATUS_ACTIVE = 10;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+            [
+                'class' => BlameableBehavior::className(),
+                'createdByAttribute' => 'user_id',
+                'updatedByAttribute' => false,
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'body'], 'required'],
+            [['body'], 'string'],
+            [['status'], 'default', 'value' => self::STATUS_ACTIVE],
+            [['status'], 'integer'],
+            [['title', 'link'], 'string', 'max' => 255],
+            [['link'], 'url', 'skipOnEmpty' => true],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'title' => Yii::t('app', 'Title'),
+            'body' => Yii::t('app', 'Body'),
+            'link' => Yii::t('app', 'Link'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'status' => Yii::t('app', 'Status'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @return string label for current status
+     */
+    public function getStatusLabel()
+    {
+        return ArrayHelper::getValue(static::getStatuses(), $this->status);
+    }
+
+    /**
+     * @return array status labels indexed by status values
+     */
+    public static function getStatuses()
+    {
+        return [
+            self::STATUS_DELETED => Yii::t('user', 'Delete'),
+            self::STATUS_ACTIVE => Yii::t('user', 'Active'),
+        ];
+    }
+}
