14208b03be455d084f019f204b74b216d85bd153
Alexander Makarov <sam@rmcreative.ru>
2015-08-13 23:01:44 +0300

    Added i18n messages config

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,16 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'fileMap' => [
+                        'site' => 'site.php',
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
