c05f0d880da11396639ea778ae8763a81822ee76
Alexander Makarov <sam@rmcreative.ru>
2015-08-14 01:18:50 +0300

    Redirect 1.1 guide and blog to yiiframework.com

---
+++ frontend/components/RedirectUrlRule.php
@@ -0,0 +1,31 @@
+<?php
+namespace frontend\components;
+
+use Yii;
+use yii\web\UrlRuleInterface;
+
+/**
+ * Redirects 1.1 guide URLs to yiiframework.com
+ */
+class RedirectUrlRule implements UrlRuleInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function parseRequest($manager, $request)
+    {
+        $url = $request->getUrl();
+        if (preg_match('~^/(?:doc/guide|doc/blog)~', $url)) {
+            Yii::$app->response->redirect('http://www.yiiframework.com' . $url, 301);
+        }
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function createUrl($manager, $route, $params)
+    {
+        return false;
+    }
+}
--- frontend/config/urls.php
+++ frontend/config/urls.php
@@ -1,4 +1,4 @@
 <?php
 return [
-
+    ['class' => 'frontend\components\RedirectUrlRule'],
 ];
