a5c1577f100003f75d37a8048366ff4e8dfc0192
Alexander Makarov <sam@rmcreative.ru>
2015-08-13 23:33:36 +0300

    Migration for post table

---
+++ console/migrations/m150813_202825_post.php
@@ -0,0 +1,27 @@
+<?php
+
+use common\components\Migration;
+
+class m150813_202825_post extends Migration
+{
+    public function up()
+    {
+        $this->createTable('post', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'body' => 'MEDIUMTEXT NOT NULL',
+            'link' => $this->string()->notNull(),
+            'user_id' => $this->integer()->notNull(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+
+        $this->addForeignKey('fk-post-user_id-user-id', 'post', 'user_id', 'user', 'id');
+    }
+
+    public function down()
+    {
+        $this->dropTable('post');
+    }
+}
