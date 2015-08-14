9c894a3fd499f248d6d446237adc8137eab128a9
Revin Roman <xgismox@gmail.com>
2015-08-14 21:36:35 +0300

    Integrate theme

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -19,6 +19,8 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/theme.css',
+        'css/theme-skin.css',
     ];
     public $js = [
     ];
