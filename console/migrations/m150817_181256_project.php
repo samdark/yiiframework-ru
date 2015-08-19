62d811081166dfbdb5f518d60b67be5d7f4c17c2
Revin Roman <xgismox@gmail.com>
2015-08-19 20:16:14 +0300

    Migration refactoring

--- console/migrations/m150817_181256_project.php
+++ console/migrations/m150817_181256_project.php
@@ -4,6 +4,7 @@ use common\components\Migration;
 
 class m150817_181256_project extends Migration
 {
+
     public function up()
     {
         $this->createTable('project', [
@@ -11,7 +12,7 @@ class m150817_181256_project extends Migration
             'title' => $this->string()->notNull(),
             'link' => $this->string()->notNull(),
             'body' => 'MEDIUMTEXT NOT NULL',
-            'user_id' => $this->integer()->notNull(),
+            'user_id' => $this->integer(),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
@@ -19,16 +20,19 @@ class m150817_181256_project extends Migration
 
         $this->createTable('project_image', [
             'id' => $this->primaryKey(),
-            'project_id' => $this->integer()->notNull(),
-            'name'=>$this->string(32)->notNull(),
+            'project_id' => $this->integer(),
+            'name' => $this->string(32)->notNull(),
         ]);
 
-        $this->addForeignKey('fk-project-user_id-user-id', 'project', 'user_id', 'user', 'id');
-        $this->addForeignKey('fk-image-project_id-project-id', 'project_image', 'project_id', 'project', 'id');
+        $this->addForeignKey('fk-project-user_id-user-id', 'project', 'user_id', 'user', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk-image-project_id-project-id', 'project_image', 'project_id', 'project', 'id', 'CASCADE', 'CASCADE');
     }
 
     public function down()
     {
+        $this->dropForeignKey('fk-image-project_id-project-id', 'project_image');
+        $this->dropForeignKey('fk-project-user_id-user-id', 'project');
+
         $this->dropTable('project_image');
         $this->dropTable('project');
     }
