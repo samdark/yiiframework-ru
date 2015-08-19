48737e78fe4340c745259ce8a5a3b46477e4c041
Евгений Андреевич <lavrentiev.e@gmail.com>
2015-08-19 20:57:12 +0300

    Контроллер для профилей

---
+++ frontend/controllers/ProfileController.php
@@ -0,0 +1,132 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use frontend\models\User;
+use yii\filters\AccessControl;
+use yii\web\NotFoundHttpException;
+use yii\data\ActiveDataProvider;
+use frontend\models\ChangePasswordForm;
+
+class ProfileController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['index', 'update'],
+                'rules' => [
+                    [
+                        'actions' => ['index', 'update'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actionIndex()
+    {
+        $profile = User::find()
+        ->where([
+            'user.id' => Yii::$app->user->identity->getId(),
+            'user.status' => User::STATUS_ACTIVE
+        ])
+        ->one();
+
+        if ($profile === null) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render('index', [
+            'profile' => $profile
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actionUpdate()
+    {
+        $profile = User::find()
+            ->where([
+                'user.id' => Yii::$app->user->identity->getId(),
+                'user.status' => User::STATUS_ACTIVE
+            ])
+            ->one();
+
+        if ($profile === null) {
+            throw new NotFoundHttpException();
+        }
+
+        $profile->scenario = User::SCENARIO_UPDATE;
+
+        $modelChangePassword = new ChangePasswordForm();
+
+        if ($profile->load(Yii::$app->request->post()) && $profile->save()) {
+            $this->redirect(['index']);
+        }
+
+        if ($modelChangePassword->load(Yii::$app->request->post()) && $modelChangePassword->change()) {
+            $this->redirect(['index']);
+        }
+
+        return $this->render('update', [
+            'profile' => $profile,
+            'modelChangePassword' => $modelChangePassword
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actionView($id)
+    {
+        $profile = User::find()
+            ->where([
+                'user.id' => $id,
+                'user.status' => User::STATUS_ACTIVE
+            ])
+            ->one();
+
+        if ($profile === null) {
+            throw new NotFoundHttpException('User does not exist');
+        }
+
+        return $this->render('view', [
+            'profile' => $profile
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actionList()
+    {
+        $query = User::find()
+            ->where([
+                'user.status' => User::STATUS_ACTIVE
+            ])
+            ->orderBy('user.created_at DESC');
+
+        $provider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => User::PAGE_COUNT,
+            ],
+        ]);
+
+        return $this->render('list', [
+            'provider' => $provider
+        ]);
+    }
+}
