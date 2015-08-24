bdd1c932934f0297afca46e68679ea35b1b759e1
githubjeka <et.coder@gmail.com>
2015-08-24 14:42:38 +0300

    sketch QA

---
+++ common/models/Question.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "question".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $title
+ * @property string $body
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ *
+ * @property Answer[] $answers
+ * @property User $user
+ */
+class Question extends ActiveRecord
+{
+    const STATUS_DELETED = 0;
+    const STATUS_ACTIVE = 10;
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+            [
+                'class' => BlameableBehavior::className(),
+                'createdByAttribute' => 'user_id',
+                'updatedByAttribute' => false,
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'body',], 'required'],
+            [['status'], 'default', 'value' => self::STATUS_ACTIVE],
+            [['status'], 'integer'],
+            [['body'], 'string'],
+            [['title'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'title' => Yii::t('app', 'Title'),
+            'body' => Yii::t('app', 'Body'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'status' => Yii::t('app', 'Status'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAnswers()
+    {
+        return $this->hasMany(Answer::className(), ['question_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
---
+++ console/migrations/m150824_110742_qa.php
@@ -0,0 +1,53 @@
+<?php
+
+use common\components\Migration;
+
+class m150824_110742_qa extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'question',
+            [
+                'id' => $this->primaryKey(),
+                'user_id' => $this->integer()->notNull(),
+                'title' => $this->string()->notNull(),
+                'body' => 'MEDIUMTEXT NOT NULL',
+                'status' => $this->smallInteger()->notNull()->defaultValue(10),
+                'created_at' => $this->integer()->notNull(),
+                'updated_at' => $this->integer()->notNull(),
+            ]
+        );
+
+        $this->createTable(
+            'answer',
+            [
+                'id' => $this->primaryKey(),
+                'user_id' => $this->integer()->notNull(),
+                'question_id' => $this->integer()->notNull(),
+                'body' => 'MEDIUMTEXT NOT NULL',
+                'status' => $this->smallInteger()->notNull()->defaultValue(10),
+                'created_at' => $this->integer()->notNull(),
+                'updated_at' => $this->integer()->notNull(),
+            ]
+        );
+
+        $this->addForeignKey('fk-question-user_id-user-id', 'question', 'user_id', 'user', 'id');
+        $this->addForeignKey('fk-answer-user_id-user-id', 'answer', 'user_id', 'user', 'id');
+        $this->addForeignKey(
+            'fk-answer-question_id-question-id',
+            'answer',
+            'question_id',
+            'question',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('answer');
+        $this->dropTable('question');
+    }
+}
