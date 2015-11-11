e7521c2b6e20ddf3c0fba582d8a5d31bd2ddde70
githubjeka <et.coder@gmail.com>
2015-11-11 21:40:26 +0300

    new design for SignUp page

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \frontend\models\SignupForm */
@@ -7,35 +6,46 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Signup';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = Yii::t('app', 'Signup');
+$this->blocks['body-class'] = 'bg-textured';
 ?>
-<div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to signup:</p>
+<div class="container-fluid login-reg">
+    <div class="container cont-border">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+                <div class="p-login-title"><?= Yii::t('app', 'Registration') ?></div>
+                <div class="p-reg-win">
 
-    <div class="row">
-        <div class="col-lg-4">
-            <?= yii\authclient\widgets\AuthChoice::widget([
-                'baseAuthUrl' => ['site/auth'],
-                'popupMode' => false,
-            ]) ?>
-        </div>
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+                    <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-                <?= $form->field($model, 'username') ?>
+                    <?= $form->field($model, 'username')
+                        ->textInput(['class' => 'form-control input-lg', 'placeholder' => $model->getAttributeLabel('username')])
+                        ->label(false) ?>
 
-                <?= $form->field($model, 'email') ?>
+                    <?= $form->field($model, 'email')
+                        ->textInput(['class' => 'form-control input-lg', 'placeholder' => $model->getAttributeLabel('email')])
+                        ->label(false) ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                    <?= $form->field($model, 'password')
+                        ->passwordInput(['class' => 'form-control input-lg', 'placeholder' => $model->getAttributeLabel('password')])
+                        ->label(false) ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                    <div class="form-group">
+                        <?= Html::submitButton(Yii::t('app', 'Signup'), ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'signup-button']) ?>
+                    </div>
+
+                    <?php ActiveForm::end(); ?>
+
                 </div>
-
-            <?php ActiveForm::end(); ?>
+                <div class="p-login-social">
+                    <div class="login-social">
+                        <a href="https://github.com/yiisoft/yii2" class="github" target="_blank"></a>
+                        <a href="https://twitter.com/yiiframework_ru" class="twitter" target="_blank"></a>
+                        <a href="https://www.facebook.com/groups/yiitalk/" class="facebook" target="_blank"></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
