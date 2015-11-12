68b74581ec95c19cbd5d9b501745b690d5705997
githubjeka <et.coder@gmail.com>
2015-11-12 15:35:56 +0300

    Add js scripts (sticky, ThreeDots)

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -1,30 +1,20 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
-
 namespace frontend\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-//        'http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|PT+Sans:400,600,700',
         'css/main.css',
 
     ];
     public $js = [
         'js/qa.js',
         'js/jquery.sticky.js',
+        'js/jquery.ThreeDots.min.js',
         'js/yiiscript.js',
     ];
     public $depends = [
