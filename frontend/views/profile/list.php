f904e073612c0af824450b832d669af428c8c801
githubjeka <et.coder@gmail.com>
2015-11-18 21:04:35 +0300

    New design for profile list

--- frontend/views/profile/list.php
+++ frontend/views/profile/list.php
@@ -4,31 +4,44 @@
 /* @var $provider yii\data\ActiveDataProvider */
 /* @var $user common\models\User */
 
+use yii\helpers\Html;
 use yii\widgets\LinkPager;
 
 $this->title = Yii::t('app', 'Member List');
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="profile-list">
-    <h1><?= Yii::t('app', 'Member List') ?></h1>
 
-    <table class="table table-bordered table-responsive table-striped table-hover">
-        <thead>
-        <tr>
-            <th><?= Yii::t('app', 'Username') ?></th>
-            <th><?= Yii::t('app', 'Registration') ?></th>
-            <th>---</th>
-        </tr>
-        </thead>
-        <tbody>
+<div class="container page-wrapper page-cont-col">
+    <div class="row">
+
         <?php foreach ($provider->getModels() as $user): ?>
-            <?= $this->render('_user', ['user' => $user]) ?>
+            <div class="col-sm-6 col-md-3">
+                <div class="c-user-item">
+                    <a href="user_profile.html" class="c-user-avatar">
+                        <?= \common\widgets\Gravatar::widget([
+                            'email' => Html::encode($user->email),
+                            'size' => 160,
+                            'options' => [
+                                'title' => Html::encode($user->username),
+                                'alt' => Html::encode($user->username)
+                            ]
+                        ]) ?>
+                    </a>
+                    <a href="user_profile.html" class="c-user-name"><?= Html::encode($user->username) ?></a>
+
+                    <div class="c-user-info">
+                        <span class="name">Никнейм</span><span class="info">Sam Dark</span><br>
+                        <span class="name">Город</span><span class="info">Воронеж</span><br>
+                        <span class="name"><?= Yii::t('app', 'Registration') ?>:</span><span
+                            class="info"><?= Yii::$app->formatter->asDate($user->created_at) ?></span>
+                    </div>
+                </div>
+            </div>
         <?php endforeach ?>
-        </tbody>
-    </table>
+
+    </div>
 
     <?= LinkPager::widget([
-        'options' => ['class' => 'pagination pagination-sm'],
+        'options' => ['class' => 'pagination'],
         'pagination' => $provider->getPagination(),
     ]) ?>
 
