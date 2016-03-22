80264586bdf854da96ccd80f21ed48445d57173a
Carsak <Carsak@users.noreply.github.com>
2016-03-22 14:59:39 +0600

    issue #37 JS для избранных вопросов

--- frontend/web/js/qa.js
+++ frontend/web/js/qa.js
@@ -2,3 +2,32 @@ function answer_reply(answer_id)
 {
     $("#answer-parent_id").val(answer_id);
 }
+
+$(function () {
+    $('.q-info-like > a').click(function () {
+        var $link = $(this),
+            $parent = $link.closest('.q-info-like');
+        $.ajax({
+            url: $link.attr('href'),
+            method: "get",
+            // TODO Лучше реализовать через Post
+            //data: {
+            //    id: $link.data('question-id'),
+            //    _csrf: yii.getCsrfToken()
+            //},
+            //context: clickElement,
+            success: function (status) {
+                if (status == 1) {
+                    $parent.addClass('active');
+                } else {
+                    $parent.removeClass('active');
+                }
+
+                $parent.children('span').text(function () {
+                    return parseInt(this.innerText) + parseInt(status);
+                });
+            }
+        });
+        return false;
+    });
+});
