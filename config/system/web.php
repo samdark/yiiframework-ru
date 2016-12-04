b771df3f65db01191b642769a51c7779ce72b952
Alexander Makarov <sam@rmcreative.ru>
2016-12-05 02:24:57 +0300

    Send emails for real in non-debug mode

--- config/system/web.php
+++ config/system/web.php
@@ -55,10 +55,7 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@app/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => YII_DEBUG,
         ],
         'assetManager' => [
             'linkAssets' => true,
