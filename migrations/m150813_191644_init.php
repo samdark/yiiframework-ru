a542b9c7f395be89b436b0f007028414ebd11ea2
Alexander Makarov <sam@rmcreative.ru>
2016-12-04 23:32:07 +0300

    MySQL unique index fix

--- migrations/m150813_191644_init.php
+++ migrations/m150813_191644_init.php
@@ -6,13 +6,15 @@ class m150813_191644_init extends Migration
 {
     public function up()
     {
+        $isMySQL = $this->getDb()->getDriverName() === 'mysql';
+
         $this->createTable('user', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string(),
-            'email' => $this->string(200)->unique(),
+            'email' => $this->string(),
             'email_verified' => $this->boolean(),
             'github' => $this->string(),
             'site' => $this->string(),
@@ -21,6 +23,8 @@ class m150813_191644_init extends Migration
             'updated_at' => $this->integer()->notNull(),
         ]);
 
+        $this->createIndex('idx-user-username-unique', '{{%user}}', $isMySQL ? 'username(191)' : 'username', true);
+        $this->createIndex('idx-user-email-unique', '{{%user}}', $isMySQL ? 'email(191)' : 'email', true);
 
         $this->createTable('auth', [
             'id' => $this->primaryKey(),
