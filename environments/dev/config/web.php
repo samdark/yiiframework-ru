69bbb9ee667bcbbc0ba728da9ba16e5830367011
Alexander Makarov <sam@rmcreative.ru>
2016-12-05 19:11:06 +0300

    Preparations for using rollbar

--- environments/dev/config/web.php
+++ environments/dev/config/web.php
@@ -5,12 +5,6 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
-        'authClientCollection' => [
-            'class' => 'yii\authclient\Collection',
-            'clients' => [
-
-            ],
-        ],
     ],
 ];
 
--- environments/prod/config/web.php
+++ environments/prod/config/web.php
@@ -1,15 +1,19 @@
 <?php
 $config = [
+    'bootstrap' => [
+        'rollbar',
+    ],
     'components' => [
+        'rollbar' => [
+            'class' => 'baibaratsky\yii\rollbar\Rollbar',
+            'accessToken' => '',
+        ],
+        'errorHandler' => [
+            'class' => 'baibaratsky\yii\rollbar\web\ErrorHandler',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
-        ],
-        'authClientCollection' => [
-            'class' => 'yii\authclient\Collection',
-            'clients' => [
-
-            ],
         ],
     ],
 ];
