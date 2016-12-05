26653688a746af03d6e7a0b2cc3b0434dfb0b6d8
Alexander Makarov <sam@rmcreative.ru>
2016-12-05 18:55:08 +0300

    Fixed validation

--- forms/PasswordResetRequestForm.php
+++ forms/PasswordResetRequestForm.php
@@ -21,7 +21,7 @@ class PasswordResetRequestForm extends Model
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'exist',
-                'targetClass' => '\app\models\user\User',
+                'targetClass' => '\app\models\User',
                 'filter' => ['status' => User::STATUS_ACTIVE],
                 'message' => 'There is no user with such email.'
             ],
