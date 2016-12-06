d770cb1714972fbe932f534e4bf0d1e381bb0a23
Alexander Makarov <sam@rmcreative.ru>
2016-12-06 23:07:23 +0300

    Do not duplicate post title

--- views/post/view.php
+++ views/post/view.php
@@ -12,10 +12,6 @@ use yii\helpers\Markdown;
 $this->title = Html::encode($post->title);
 ?>
 <article class="post-item">
-    <div class="post-title">
-        <?= Html::encode($post->title) ?>
-    </div>
-
     <div class="post-info">
         <?= Yii::t('post', 'Date publication:') ?> <?= Yii::$app->formatter->asDatetime($post->updated_at, 'short'); ?><span class="margin-line">|</span>
         <?= Yii::t('post', 'Author:') ?> <?= Html::a(Html::encode($post->user->username), ['/user/view', 'id' => $post->user->id, 'username' => $post->user->username]); ?>
