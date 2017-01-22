825ce8debe09fa0dc527a32b796f3e008666ac99
Pavel Ivanov <rugabarbo@gmail.com>
2017-01-22 18:47:57 +0200

    PhpDoc для Issue #105: поможет подсвечивать вызовы Yii::t() в IDE как deprecated (#111)

--- autocompletion.php
+++ autocompletion.php
@@ -11,6 +11,20 @@ class Yii extends \yii\BaseYii
      * @var BaseApplication|WebApplication|ConsoleApplication
      */
     public static $app;
+
+    /**
+     * Все новые строки следует задавать на русском языке без использования функции Yii::t(),
+     * а все имеющиеся вызовы Yii::t() постепенно удалять в пользу прямого использования русских строк.
+     *
+     * Ниже дана ссылка на соответствующий issue на GitHub.
+     *
+     * @deprecated
+     * @see https://github.com/samdark/yiiframework-ru/issues/105
+     *
+     * @inheritdoc
+     * @see BaseYii::t()
+     */
+    public static function t($category, $message, $params=[], $language=null) {}
 }
 
 abstract class BaseApplication extends yii\base\Application
