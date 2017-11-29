3ff6a19578fc529e681efc92cabd13b947e321cf
Alexander Makarov <sam@rmcreative.ru>
2017-11-29 21:11:18 +0300

    Updated versions

--- config/system/versions.php
+++ config/system/versions.php
@@ -2,6 +2,6 @@
 return [
     'yii1-tag-name' => '1.1.19',
     'yii1-html-url' => 'https://github.com/yiisoft/yii/releases/tag/1.1.19',
-    'yii2-tag-name' => '2.0.12',
-    'yii2-html-url' => 'https://github.com/yiisoft/yii2/releases/tag/2.0.12',
+    'yii2-tag-name' => '2.0.13',
+    'yii2-html-url' => 'https://github.com/yiisoft/yii2/releases/tag/2.0.13',
 ];
