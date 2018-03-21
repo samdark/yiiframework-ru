7a82f880e0973e1e5b4e63722be237775559f212
Alexander Makarov <sam@rmcreative.ru>
2018-03-21 03:08:31 +0300

    Update links to point to new official website

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -69,8 +69,8 @@ AppAsset::register($this);
             <?php
             $menuItems = [
                 ['label' => '1.1', 'url' => ['/site/legacy']],
-                ['label' => 'Руководство', 'url' => 'http://stuff.cebe.cc/yii2docs-ru/guide-README.html', 'linkOptions' => ['target' => '_blank', 'rel' => 'noopener noreferrer']],
-                ['label' => 'API', 'url' => 'http://stuff.cebe.cc/yii2docs-ru/index.html', 'linkOptions' => ['target' => '_blank', 'rel' => 'noopener noreferrer']],
+                ['label' => 'Руководство', 'url' => 'https://new.yiiframework.com/doc/guide/2.0/ru', 'linkOptions' => ['target' => '_blank', 'rel' => 'noopener noreferrer']],
+                ['label' => 'API', 'url' => 'https://new.yiiframework.com/doc/api/2.0', 'linkOptions' => ['target' => '_blank', 'rel' => 'noopener noreferrer']],
                 ['label' => 'Расширения', 'url' => 'https://yiigist.com/', 'linkOptions' => ['target' => '_blank', 'rel' => 'noopener noreferrer']],
 //                [
 //                    'label' => 'Вопросы',
