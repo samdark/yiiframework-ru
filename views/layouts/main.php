e0d02d810c5cb4d4c682eb6a825af82f7f167162
Alexander Makarov <sam@rmcreative.ru>
2021-07-06 12:07:15 +0300

    Remove PHP Russia announcement

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,7 +23,9 @@ AppAsset::register($this);
 <body class="<?= $this->blocks['body-class'] ?? '' ?>">
 <?php $this->beginBody() ?>
 
+<?php /*
 <div class="important-message"><a href="https://phprussia.ru/moscow/2020/">Конференция PHP Russia</a>. Оффлайн 28 июня 2021, Москва.</div>
+*/ ?>
 
 <nav class="navbar navbar-default" role="navigation">
     <div class="container">
